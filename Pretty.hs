e8e08a15dc3ffcd79d811ae48bdf10ba3221421e
Mabry Cervin <mpcervin@uncg.edu>
2015-03-10 16:31:20 -0400

    Added toy kernel printing function

--- Pretty.hs
+++ Pretty.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Pretty where
 
 import Syntax
 import Text.PrettyPrint
-import Data.Text hiding (empty)
+import Data.Text hiding (empty, reverse, head)
 
 -- | Takes the parameter list and formats it
 --   to OpenCL syntax. Input parameters are
@@ -19,3 +20,20 @@ paramList ((Input t n):xs) =
 flattenVecExpr :: VecExpr -> Doc
 flattenVecExpr (Vec n _) = text $ (unpack n) ++ "[gid]"
 flattenVecExpr (Add a b) = (flattenVecExpr a) <+> (text "+") <+> (flattenVecExpr b)
+
+-- | Toy implementation of the final printer.
+kernelPrinter :: Kernel -> Doc
+kernelPrinter (Kernel name params vecexpr) =
+        let
+            signature = "__kernel void" <+> txt name <> "(" <> (paramList params) <> ") {"
+            gid = "int gid = get_global_id(0);"
+            getname (Output _ n) = n
+            outputname = getname . head . reverse
+            vecops = (txt $ outputname params) <+> "=" <+> (flattenVecExpr vecexpr) <> ";"
+        in
+            signature $+$ (nest 4 gid) $+$ (nest 4 vecops) $+$ "}"
+
+-- | Helper function for going directly from
+--   Text to Doc
+txt :: Text -> Doc
+txt = text . unpack
